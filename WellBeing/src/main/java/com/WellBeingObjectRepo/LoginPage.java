a014f43b0d2a9035b21f95c95163e049d8820b80
prave <prave@DESKTOP-S40FGN7>
2023-03-03 17:14:06 +0530

    Updated Package

--- WellBeing/src/main/java/com/WellBeingObjectRepo/LoginPage.java
+++ WellBeing/src/main/java/com/WellBeingObjectRepo/LoginPage.java
@@ -1,7 +1,10 @@
 package com.WellBeingObjectRepo;
 
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.Select;
 
 public class LoginPage {
 @FindBy(id = "FacilityId") private WebElement Facilitydropdown;
@@ -9,7 +12,28 @@ public class LoginPage {
 @FindBy(id = "Password") private WebElement passwordTextField;
 @FindBy(xpath = "//button[@id='loginBtn']") private WebElement SigninButton;
 
-
+public LoginPage(WebDriver driver)
+{
+	PageFactory.initElements(driver, this);
+}
+public void selectDropdown()
+{
+	Select s=new Select(Facilitydropdown);
+	s.selectByIndex(1);
+}
+public void enterLoginId(String id)
+{
+	idTextField.sendKeys(id);
+}
+public void pwd(String password)
+{
+	passwordTextField.sendKeys(password);
+}
+public void signButton()
+{
+	SigninButton.click();
+	
+}
 	
 	
 
