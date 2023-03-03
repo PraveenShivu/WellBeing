f46e38823f289f602e4a5b159f9fca286efb555e
prave <prave@DESKTOP-S40FGN7>
2023-03-03 15:59:51 +0530

    New Package

--- WellBeing/src/main/java/com/WellBeingObjectRepo/LoginPage.java
+++ WellBeing/src/main/java/com/WellBeingObjectRepo/LoginPage.java
@@ -1,6 +1,15 @@
 package com.WellBeingObjectRepo;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class LoginPage {
+@FindBy(id = "FacilityId") private WebElement Facilitydropdown;
+@FindBy(id = "LoginId") private WebElement idTextField;
+@FindBy(id = "Password") private WebElement passwordTextField;
+@FindBy(xpath = "//button[@id='loginBtn']") private WebElement SigninButton;
+
+
 	
 	
 
