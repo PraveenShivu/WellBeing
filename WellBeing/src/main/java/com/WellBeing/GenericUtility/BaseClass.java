ed45cb40ac75d6841ef279f5b32781ae790b7f72
prave <prave@DESKTOP-S40FGN7>
2023-03-08 14:59:08 +0530

    edit

--- WellBeing/src/main/java/com/WellBeing/GenericUtility/BaseClass.java
+++ WellBeing/src/main/java/com/WellBeing/GenericUtility/BaseClass.java
@@ -13,53 +13,34 @@ public class BaseClass {
 	protected  WebDriver driver;
 	public  WebDriver sdriver;
 	protected ReadDataFromProperty Property;
-
-
-	
-
 	protected LoginPage Log;
 	protected CommonPage Com;
 	protected JavaUtility java;
 	protected WebdriverUtility wd;
 	protected ReadDataFromExcel exl;
 
-
-
 	@BeforeClass
 	public void Launch()
 	{
 		Property=new ReadDataFromProperty();
 		java=new JavaUtility();
-
 		wd=new WebdriverUtility();
 		exl=new ReadDataFromExcel();
+
+		Log=new LoginPage(driver);
+		Com=new CommonPage(driver);
 
 		String Browser = Property.getPropertyData(PropertyFileKeys.BROWSER.getKey());
 		String Url = Property.getPropertyData(PropertyFileKeys.URL.getKey());
 		String Time = Property.getPropertyData(PropertyFileKeys.TIMEOUT.getKey());
 		Long timeWait = (Long)java.stringToAnyDataType(Time, "long");
-
-		
-
-
 		sdriver=driver;
 		driver=wd.LaunchApp(Browser, Url,timeWait);
-
-        Log=new LoginPage(driver);
-
-   
-
-		
-		
-		Log=new LoginPage(driver);
-
-		Com=new CommonPage(driver);
-
 	}
+
 	@BeforeMethod
 	public void Login()
-	{ 
-		
+	{ 		
 		LoginPage log =new LoginPage(driver);
 		String option = Property.getPropertyData(PropertyFileKeys.OPTION.getKey());
 		System.out.println(option);
@@ -67,28 +48,20 @@ public class BaseClass {
 		System.out.println(Id);
 		String Password = Property.getPropertyData(PropertyFileKeys.PASSWORD.getKey());
 
-
-		System.out.println(Password);
-
-
-		
-		
-
 		Log.facilityClick();
-
 		Log.enterLoginId(Id);
 		Log.pwd(Password);
 		Log.signButton();
-		
+
 	}
 	@AfterMethod
 	public void Logout()
 	{
 		driver.quit();
-//		Com.logOutApp();
-//		WebElement w1 = Com.returnLog();
-//		wd.waitTillVisible(w1);
-//		Com.LogOut();
+		//		Com.logOutApp();
+		//		WebElement w1 = Com.returnLog();
+		//		wd.waitTillVisible(w1);
+		//		Com.LogOut();
 	}
 
 }
