8f52ea4bf4c21f80ad430e2e3ced20018ea9c74c
ShivaPD <shivakumardhulehole@gmail.com>
2023-03-08 11:14:09 +0530

    8-03-23 First

--- WellBeing/src/main/java/com/WellBeing/GenericUtility/BaseClass.java
+++ WellBeing/src/main/java/com/WellBeing/GenericUtility/BaseClass.java
@@ -48,8 +48,7 @@ public class BaseClass {
 		String option = Property.getPropertyData(PropertyFileKeys.OPTION.getKey());
 		String Id = Property.getPropertyData(PropertyFileKeys.LOGINID.getKey());
 		String Password = Property.getPropertyData(PropertyFileKeys.PASSWORD.getKey());
-		//WebElement dpdn11 = Log.facilityClick();
-		//	wd.drop(dpdn11, option);
+		
 		Log.facilityClick();
 		Log.enterLoginId(Id);
 		Log.pwd(Password);
@@ -59,10 +58,11 @@ public class BaseClass {
 	@AfterMethod
 	public void Logout()
 	{
-		Com.logOutApp();
-		WebElement w1 = Com.returnLog();
-		wd.waitTillVisible(w1);
-		Com.LogOut();
+		driver.quit();
+//		Com.logOutApp();
+//		WebElement w1 = Com.returnLog();
+//		wd.waitTillVisible(w1);
+//		Com.LogOut();
 	}
 
 }
